a77033233019d797158f04cd3e1c780dc5eb19be
Łukasz Zięba <36661799+lzieba91@users.noreply.github.com>
2018-06-20 12:28:45 +0200

    Add files via upload

---
+++ Default.aspx
@@ -0,0 +1,18 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+
+            <asp:Button ID="Button" runat="server" Text="Start testing" OnClick="Button_Click" />
+
+        </div>
+    </form>
+</body>
+</html>
---
+++ Default.aspx.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Threading;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+       
+    }
+
+    public static void ConsumeCPU(int percentage)
+    {
+        if (percentage < 0 || percentage > 100)
+            throw new ArgumentException("percentage");
+        Stopwatch watch = new Stopwatch();
+        watch.Start();
+        while (true)
+        {
+            // Make the loop go on for "percentage" milliseconds then sleep the 
+            // remaining percentage milliseconds. So 40% utilization means work 40ms and sleep 60ms
+            if (watch.ElapsedMilliseconds > percentage)
+            {
+                Thread.Sleep(100 - percentage);
+                watch.Reset();
+                watch.Start();
+            }
+        }
+    }
+
+    protected void Button_Click(object sender, EventArgs e)
+    {
+        ConsumeCPU(100);
+    }
+}
